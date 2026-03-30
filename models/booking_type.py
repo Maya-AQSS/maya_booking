78650612a6a31f7fc18b25a9fae5f154e6476bc6
Pau-Munoz <paumunale2@alu.edu.gva.es>
2026-03-30 17:05:58 +0200

    Descomentado y arreglada relacion entre modelos

--- models/booking_type.py
+++ models/booking_type.py
@@ -20,7 +20,7 @@ class BookingType(models.Model):
         ('W', 'Puesto'),     
         ('I', 'Elemento de inventario'), ], 
         default='S') 
-    # resource_ids = fields.One2many('booking.type.resource', 'type_id') 
+    resource_ids = fields.One2many('maya_booking.booking_type_resource', 'type_id')
 
     # #  computed para mostrar cuántos recursos tiene asociados
     # resource_count = fields.Integer(compute='_compute_resource_count')
--- models/booking_type_resource.py
+++ models/booking_type_resource.py
@@ -11,7 +11,7 @@ class BookingTypeResource(models.Model):
     _name = 'maya_booking.booking_type_resource'
     _description = 'Recursos Reservables'
 
-    # type_id = fields.Many2one('maya_booking.booking_type', ondelete='cascade') 
-    # reservable_model = fields.Char(required=True)      # ej: 'maya_core.employee', 
-    # reservable_id = fields.Integer(required=True)   # el id del registro al que queremos apuntar
+    type_id = fields.Many2one('maya_booking.booking_type', ondelete='cascade') 
+    reservable_model = fields.Char(required=True)      # ej: 'maya_core.employee', 
+    reservable_id = fields.Integer(required=True)   # el id del registro al que queremos apuntar
     # reservable_ref = fields.Reference( selection='_get_reservable_models',     compute='_compute_reservable_ref',     store=True )
