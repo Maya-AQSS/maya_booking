dbf3e655ab3ddab60d3d56e3cee91c0574d3ab1c
aoltra <aoltra@gmail.com>
2026-04-14 19:56:23 +0200

    Cambio de herencia del modelo Place
    Se ha ubicado Place en core, así que la herencia debe cambiar

--- models/reservable/place.py
+++ models/reservable/place.py
@@ -1,5 +1,4 @@
 from odoo import models, fields
 
 class Place(models.Model):
-    _name = 'maya_core.place'
-    _inherit = ['maya_booking.place', 'maya_booking.reservable.mixin']
+    _inherit = ['maya_core.place', 'maya_booking.reservable.mixin']
