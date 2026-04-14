cb52636ebd945073c839756a897c17c969096c46
aoltra <aoltra@gmail.com>
2026-04-14 19:54:27 +0200

    Renambrado del modelo 'maya_booking.place' -> 'maya_core.place'

--- models/reservable/place.py
+++ models/reservable/place.py
@@ -1,5 +1,5 @@
 from odoo import models, fields
 
 class Place(models.Model):
-    _name = 'maya_booking.place'
+    _name = 'maya_core.place'
     _inherit = ['maya_booking.place', 'maya_booking.reservable.mixin']
